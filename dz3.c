f8cae49fad0c523e9e8faedac6867437ae9d1a15
SvirkoA <94311402+SvirkoA@users.noreply.github.com>
2022-04-16 10:09:24 +0300

    Update dz3.c

--- dz3.c
+++ dz3.c
@@ -1,116 +1,76 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
 
-int main()
-{
-    FILE *input, *output, *output2;
-    char a, s[30][30];
-    char s2[30][30];
-    int i = 0, j = 0, count = 0, b = 0, v = 0;
+int main(){
+  int n, i, j;
+  int y =0;
+  int z =0;
+  int A[101][101];
+  FILE* flita, *flita1;
+  flita = fopen("flita.txt", "r");
+  scanf("%d", &n);
+  for (i = 0; i < n; i++){
+    for (j = 0; j < n; j++){
+      fscanf(flita, "%d", &A[i][j]);
+    }
+  }
+fclose(flita);
 
-    input = fopen("input.txt", "r"); 
-    while (!feof(input)) {
-        fscanf(input, "%c", &a);
-        if (a == '1' || a == '0') { 
-            s[i][j] = a;
-            j++;
-        }
-        if (a == '\n') { 
-            i++;
-            j = 0;
+  flita1 = fopen("flita1.txt", "w");
+  fprintf(flita1, "graph {");
+  for (i = 0; i < n; i++){
+    for (j = 0; j < n; j++){
+      if (A[i][j] != 0){
+        fprintf(flita1, "%d -- %d\n", i + 1, j + 1);
+          if (A[i][j] > 1){
+            fprintf(flita1, " [label = %d]\n", A[i][j]);
+          }
+      }
+    }
+  }
+  fprintf(flita1, " } ");
+  
+  for(i = 0;  i < n; i++){
+    for(j = 0; j < n; j++){
+      if(A[i][j] != 0 && i != j){
+        z += 1;
+      }
+
+    }
+  }
+  for(i = 0;  i < n; i++){
+    for(j = 0; j < n; j++){
+      if(A[i][j] != 0 && A[j][i] != 0 && i < j){
+            z -= 1;
         }
     }
-    for (int n = 0; n < j; n++) { 
-        count = 0;
-        for (int m = 0; m <= i; m++) {
-            if (s[m][n] == '1' && count == 0) {
-                count++;
-                b = m;
-            }
-            else if (s[m][n] == '1') {
-                count++;
-                if (count == 2) { 
-                    s2[b][m] = '1';
-                    s2[m][b] = '1';
-                }
-            }
-        }
-        if (count == 1) {
-            v = 1;
-        }
+  }
+  printf("Rebra:%d", z);
+  
+  for(i = 0;  i < n; i++){
+    int x = 0;
+    for(j = 0; j < n; j++){
+      if(A[i][j] != 0){
+        x = 1;
+      }
     }
-    output2 = fopen("output2.txt", "w");
-    for (int m = 0; m <= i; m++) {
-        fprintf(output2, "%d: ", m + 1);
-        count = 0;
-        for (int n = 0; n <= i; n++) {
-            if (s2[m][n] == '1') {
-                fprintf(output2, "%d ", n + 1);
-                count++;
-            }
-        }
-        fprintf(output2, "\n");
+    for(j = 0; j < n; j++){
+      if(A[j][i] != 0){
+        x = 1;
+      }
     }
-    for (int m = 0; m < i; m++) {
-        for (int k = m + 1; k <= i; k++) {
-            count = 0;
-            for (int n = 0; n < j; n++) {
-                if (s[m][n] == '1' && s[k][n] == '1') {
-                    count++;
-                }
-            }
-            if (count > 1) {
-                v = 1;
-            }
-        }
+    if(x != 0){
+      y += 1;
     }
-    if (v == 1) {
-        fprintf(output2, "Граф не является простым");
-    }
-    else {
-        if (j > ((i - 2) * (i - 1) / 2)) {
-            fprintf(output2, "Граф связный");
-        }
-        else {
-            fprintf(output2, "Граф несвязный");
-        }
-    }
-    fclose(output2);
-    output = fopen("output.gv", "w"); 
-    fprintf(output, "graph G {\n");
-    fprintf(output, "node [shape = circle];\n");
-    for (int n = 0; n < j; n++) {
-        count = 0;
-        for (int m = 0; m <= i; m++) {
-            if (s[m][n] == '1' && count == 0) {
-                count++;
-                b = m + 1;
-            }
-            else if (s[m][n] == '1') {
-                count++;
-                if (count == 2) {
-                    fprintf(output, "%d -- %d\n", b, m + 1);
-                }
-            }
-        }
-        if (count == 1) {
-            fprintf(output, "%d -- %d\n", b, b);
-        }
-    }
-    for (int m = 0; m <= i; m++) { 
-        count = 0;
-        for (int n = 0; n < j; n++) {
-            if (s[m][n] == '1') {
-                count++;
-            }
-        }
-        if (count == 0) { 
-            fprintf(output, "%d\n", m + 1);
-        }
-    }
-    fprintf(output, "}");
-    fclose(output);
-    system("dot output.gv -Tpng -o graphtvd.png");
-    system("graphtvd.png");
-    return 0;
+  }
+  printf("\nVershiny:%d", y);
+  int u = (y-1)*(y-2)/2;
+  if(z > u){
+    printf("\n%d > %d\nSvazannaya", z, u);
+  }
+  else{
+    printf("\n%d < %d\nNe svazannaya", z, u);
+  }
 }
